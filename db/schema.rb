a08e98602fae76e6895b8c3ebd3d3286e8eb7bd1
David <david.yang026@gmail.com>
2021-09-16 11:22:47 -0700

    created follows table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_16_181727) do
+ActiveRecord::Schema.define(version: 2021_09_16_182104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "follower_id", null: false
+    t.integer "followee_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followee_id"], name: "index_follows_on_followee_id"
+    t.index ["follower_id"], name: "index_follows_on_follower_id"
+  end
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id", null: false
