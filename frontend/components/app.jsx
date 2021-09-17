578ec861e588cf7d57c1e2466e6c284159afcbf9
David <david.yang026@gmail.com>
2021-09-17 15:25:08 -0700

    passed in NavbarContainer

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,14 +1,14 @@
 import React from "react"
 import { Route, Switch } from "react-router-dom";
-import Navbar from "./navbar/nav_bar"
-import splash from "./splash/splash";
+import NavbarContainer from "./navbar/navbar_container"
+import Splash from "./splash/splash";
 
 
 const App = () => {
     return (
     <div>
-        <Route path="/" render={ ( props ) => ( props.location.pathname !== "/") && <Navbar /> }/>
-        <Route exact path="/" component = {splash} />
+        <Route path="/" render={ ( props ) => ( props.location.pathname !== "/") && <NavbarContainer /> }/>
+        <Route exact path="/" component = {Splash} />
 
     </div>
     )}
