6419135b376317b3aead9928791b7cf3b032ddeb
David <david.yang026@gmail.com>
2021-09-17 19:26:22 -0700

    initial commit

---
+++ frontend/components/likes/likes.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+class Likes extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+
+    render(){
+        return(
+            <div>
+                <h2>likes component</h2>
+
+                {/* show all liked posts */}
+            </div>
+        )
+    }
+}
+
+export default Likes
---
+++ frontend/components/likes/likes_container.js
@@ -0,0 +1,16 @@
+import {connect} from 'react-redux'
+import Likes from "./likes"
+
+
+
+
+const mapStateToProps = state => {
+    return null
+}
+
+const mapDispatchToProps = dispatch => {
+    return null
+}
+
+
+export default connect(mapStateToProps,mapDispatchToProps)(Likes)
