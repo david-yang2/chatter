5bc611cf07a2d7f31e0bd257959eba2b2b81536f
David <david.yang026@gmail.com>
2021-09-17 19:25:48 -0700

    initial commit

---
+++ frontend/components/explore/explore.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+
+class Explore extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return (
+            <div>
+                <h2>explore section</h2>
+
+                {/* filter based on topics */}
+                
+            </div>
+        )
+    }
+
+}
+
+
+export default Explore
---
+++ frontend/components/explore/explore_container.js
@@ -0,0 +1,13 @@
+import {connect} from 'react-redux'
+import Explore from "./explore"
+
+const mapStateToProps = state => {
+    return null
+}
+
+const mapDispatchToProps = dispatch => {
+    return null
+}
+
+
+export default connect(mapStateToProps,mapDispatchToProps)(Explore)
