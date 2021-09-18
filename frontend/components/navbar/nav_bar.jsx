56babc3fffefdcf5a08d9a0ecec5817fe55e7fe1
David <david.yang026@gmail.com>
2021-09-17 21:48:09 -0700

    icons and links for navbar

--- frontend/components/navbar/nav_bar.jsx
+++ frontend/components/navbar/nav_bar.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
 import {withRouter} from "react-router-dom"
+import Navlinks from './navlinks'
+
+//Icons from react icons
+import { AiOutlineHome } from "react-icons/ai";
+import { FaHashtag } from "react-icons/fa";
+import { BsBell } from "react-icons/bs";
+import { IoPersonOutline } from "react-icons/io5";
+import { FiTwitter } from "react-icons/fi";
+
+
 
 
 class Navbar extends React.Component{
@@ -16,14 +26,19 @@ class Navbar extends React.Component{
     render(){
         return(
             <div>
-                <h3> nav bar will go here</h3>
+                <FiTwitter />
                 {/* Home */}    
-                {/* Explore */}    
+                <Navlinks Icon={AiOutlineHome} text={"Home"} />
+                {/* Explore */}   
+                <Navlinks Icon={FaHashtag} text={"Explore"} /> 
                 {/* Likes  */}    
+                <Navlinks Icon={BsBell} text={"Likes"} />
                 {/* Profile  */}    
+                <Navlinks Icon={IoPersonOutline} text={"Profile"} />
 
 
                 {/* chat button that will create a pop up form */}
+                <button> Chat </button>
 
                 {/* logout button */}
                 <button onClick={this.signout}>Log out</button>
