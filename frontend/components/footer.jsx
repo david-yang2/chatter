caf5765342a65f175cea5bb508d61e185c90c9ee
David <david.yang026@gmail.com>
2021-09-19 13:20:19 -0700

    initial commit for footer

---
+++ frontend/components/footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Footer = () =>{
+    return(
+        <div className="footer" >
+            <h3> What's Trending? </h3>
+            <h5>ExplorationAwaits</h5>
+            <h5>What's For Dinner?</h5>
+
+        </div>
+    )
+}
+
+export default Footer
