a1c39f5c18bea75f4f6d53ded3426d314978ddee
David <david.yang026@gmail.com>
2021-09-19 11:13:28 -0700

    toggle logout button

--- frontend/components/navbar/nav_bar.jsx
+++ frontend/components/navbar/nav_bar.jsx
@@ -8,6 +8,7 @@ import { FaHashtag } from "react-icons/fa";
 import { BsBell } from "react-icons/bs";
 import { IoPersonOutline } from "react-icons/io5";
 import { FiTwitter } from "react-icons/fi";
+import { BsThreeDots } from "react-icons/bs";
 
 
 
@@ -16,17 +17,35 @@ class Navbar extends React.Component{
     constructor(props){
         super(props)
         
+        this.state={
+            logoutDisplay: false
+        }
         this.signout = this.signout.bind(this)
+        this.toggleLogout = this.toggleLogout.bind(this)
     }
 
     signout(){
         this.props.logout().then(()=>this.props.history.push('/'))
     }
 
+
+    toggleLogout(){
+        // if true show logout div
+        this.setState({logoutDisplay : !this.state.logoutDisplay})
+
+    }
+
     render(){
+        let display;
+        if (this.state.logoutDisplay === true) {
+            display = (<div>
+                <button onClick={this.signout}>Log out</button>
+            </div>)
+        } else {<div></div>}
+
         return(
-            <div>
-                <FiTwitter />
+            <div className="navbar">
+                <FiTwitter size={28}/>
                 {/* Home */}    
                 <Navlinks Icon={AiOutlineHome} text={"Home"} />
                 {/* Explore */}   
@@ -38,10 +57,17 @@ class Navbar extends React.Component{
 
 
                 {/* chat button that will create a pop up form */}
-                <button> Chat </button>
+                <button className="chatbtn"> Chat </button>
 
-                {/* logout button */}
-                <button onClick={this.signout}>Log out</button>
+                {/* username */}
+                {/* <h3>{this.props.currentUser}</h3> */}
+
+
+
+                {/* logout section */}
+                {display}
+                <button onClick={this.toggleLogout}><BsThreeDots /></button>
+                
 
             </div>
 
