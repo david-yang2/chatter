9cba8f2ce0b017326b6151ea889c99ac28a1eb4d
David <david.yang026@gmail.com>
2021-09-18 22:03:55 -0700

    updated Routes for nav components

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,13 +2,21 @@ import React from "react"
 import { Route, Switch } from "react-router-dom";
 import NavbarContainer from "./navbar/navbar_container"
 import Splash from "./splash/splash";
+import ExploreContainer from "./explore/explore_container"
+import HomeContainer from "./home/home_container"
+import LikesContainer from "./likes/likes_container"
 
 
 const App = () => {
     return (
-    <div>
+    <div className = 'app'>
         <Route path="/" render={ ( props ) => ( props.location.pathname !== "/") && <NavbarContainer /> }/>
         <Route exact path="/" component = {Splash} />
+        <Route path="/home" component={HomeContainer} />
+        <Route path="/explore" component={ExploreContainer} />
+        <Route path="/likes" component={LikesContainer} />
+
+
 
     </div>
     )}
