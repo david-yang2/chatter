62a29038bb7ed9ffb207ae06e3780e5c78e330da
David <david.yang026@gmail.com>
2021-09-20 18:47:08 -0700

    post to chat

---
+++ frontend/components/chatbox/chatbox.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+
+
+
+class Chatbox extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+
+
+    render(){
+        return(
+            <div>
+                {/* form to create post */}
+                <h1>Chat box Form</h1>
+
+                {/* form has avatar, input textarea, tweet button to submit */}
+
+                
+            </div>
+        )
+
+    }
+}
+
+export default Chatbox
