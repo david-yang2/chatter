e56a36e6ee81959df41ff45a7319267f25ef576b
David <david.yang026@gmail.com>
2021-09-21 14:38:54 -0700

    set up routes for filter and search action

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,13 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
 
+    get "/explore", to: "chats#filter"
+    get "/search", to:"chats#search"
+
     resources :chats 
     resources :users
     resource :session, only: [:new, :create, :destroy]
+    
 
 
 
