c3919cc6d790b657586fa385d3473e9f7e5ea367
David <david.yang026@gmail.com>
2021-09-20 21:45:36 -0700

    created form which takes user input

--- frontend/components/chatbox/chatbox.jsx
+++ frontend/components/chatbox/chatbox.jsx
@@ -5,23 +5,37 @@ import React from "react"
 class Chatbox extends React.Component{
     constructor(props){
         super(props)
+        this.state = this.props.newChat
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+  
+    update(field){
+   
+      return e => {
+  
+        this.setState({[field]: e.currentTarget.value});
+      }
+    }
+  
+    handleSubmit(){
+  
+      this.props.submitChat(this.state)
     }
 
 
-
-    render(){
-        return(
-            <div>
-                {/* form to create post */}
-                <h1>Chat box Form</h1>
-
-                {/* form has avatar, input textarea, tweet button to submit */}
-
-                
-            </div>
-        )
-
-    }
+  render (){
+    return(
+      <form onSubmit={this.handleSubmit}>
+          <textarea
+            placeholder="What's happening?"
+            value={this.state.body}
+            onChange={this.update('body')}
+          />
+        <input type="submit" value="Chat" />
+      </form>
+    );
+  }
 }
 
 export default Chatbox
