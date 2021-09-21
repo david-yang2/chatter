eb30b251a85ce0d3717ba3e4cd9d1ae3741aeefd
David <david.yang026@gmail.com>
2021-09-20 21:28:43 -0700

    chat ordered by created_at

--- frontend/actions/chat_actions.js
+++ frontend/actions/chat_actions.js
@@ -4,6 +4,7 @@ import * as ChatApiUtil from "../util/chat_api_util"
 
 
 export const RECEIVE_ALL_CHATS = "RECEIVE_ALL_CHATS"
+export const RECEIVE_CHAT = "RECEIVE_CHAT"
 
 
 const receiveChats = chats => ({
@@ -11,8 +12,22 @@ const receiveChats = chats => ({
     chats
 })
 
+const receiveChat = chat => {
+    debugger
+    return {
+    type: RECEIVE_CHAT,
+    chat}
+}
+
 
 export const getChats = () => dispatch => {
     return ChatApiUtil.fetchChats()
     .then(chats => dispatch(receiveChats(chats)))
 };
+
+
+export const createChat = newChat => dispatch => {
+    debugger
+    return ChatApiUtil.postChat(newChat)
+    .then(newChatAdded => dispatch(receiveChats(newChatAdded)))
+}
