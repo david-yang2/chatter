0ec7e33793810c31d364f99b624fd91c07691f1f
David <david.yang026@gmail.com>
2021-09-21 22:52:22 -0700

    passed chats and search as props

---
+++ frontend/components/footer/footer_container.js
@@ -0,0 +1,23 @@
+import {connect} from "react-redux"
+import Footer from "./footer"
+import {search} from "../../actions/chat_actions"
+
+
+const mapStateToProps = state => {
+    return{
+        chats: state.chats
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    debugger
+    return {
+        search: term => dispatch(search(term))
+    }
+}
+
+
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Footer)
