b41419f7fcb860fbae5b823022d49cbd69039046
David <david.yang026@gmail.com>
2021-09-21 18:29:35 -0700

    created filter buttons and rendered feed

--- frontend/components/explore/explore.jsx
+++ frontend/components/explore/explore.jsx
@@ -1,17 +1,42 @@
 import React from "react"
+import Feed from "../feed/feed"
 
 
 class Explore extends React.Component{
     constructor(props){
         super(props)
+
+        this.filterChats = this.filterChats.bind(this)
     }
 
+    componentDidMount(){
+        this.props.getChats()
+    }
+
+    filterChats(topic){
+        this.props.filterChats(topic)
+    }
+
+
     render(){
+
+        if (Object.values(this.props.chats).length <1) return null
+
+        const {chats} = this.props
+
         return (
             <div>
                 <h2>explore section</h2>
-
+                
                 {/* filter based on topics */}
+                <button onClick={()=> this.filterChats("")}>All</button>
+                <button onClick={()=> this.filterChats("Sports")}>Sports</button>
+                <button onClick={()=> this.filterChats("Finance")}>Finance</button>
+
+                {/* <Feed /> */}
+
+                {Object.values(chats).map((chat, idx) => <Feed key = {idx} 
+                                                                chat={chat}/>)}
                 
             </div>
         )
