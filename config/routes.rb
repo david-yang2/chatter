588dd76f627cb37e4554f4e23e08c69d89a4fd4c
David <david.yang026@gmail.com>
2021-09-21 18:27:33 -0700

    fixed route for filter action

--- config/routes.rb
+++ config/routes.rb
@@ -7,17 +7,13 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
 
-    get "/explore", to: "chats#filter"
-    get "/search", to:"chats#search"
+    get "/explore",   to: "chats#getfilter"
+    post "/explore",   to: "chats#filter"
+    get "/search",    to:"chats#search"
 
     resources :chats 
     resources :users
     resource :session, only: [:new, :create, :destroy]
-    
-
-
-
-
 
   end
 end
