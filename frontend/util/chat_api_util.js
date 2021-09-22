35f2c94f7a36e5c7804bdaf1014488c33bc96fb2
David <david.yang026@gmail.com>
2021-09-21 18:31:49 -0700

    postTopic ajax request

--- frontend/util/chat_api_util.js
+++ frontend/util/chat_api_util.js
@@ -1,10 +1,23 @@
 
-
-
-
 export const fetchChats = () => {
     return $.ajax({
         url:`/api/chats`,
-        method: `GET`
+        method: `GET`,
+    })
+}
+
+export const postChat = chat => {
+    return $.ajax({
+        url:`/api/chats`,
+        method: "POST",
+        data: {chat: chat}, 
+    })
+}
+
+export const postTopic = topic => {
+    return $.ajax({
+        url:`/api/explore`,
+        method: "POST",
+        data: {filteredChat: topic}, 
     })
 }
