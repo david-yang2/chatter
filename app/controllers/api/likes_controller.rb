e1070a606786c5dc2ab2fe7f21c6fc12f4c16d49
David <david.yang026@gmail.com>
2021-09-23 11:09:00 -0700

    created userLiked action

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -1,2 +1,31 @@
 class Api::LikesController < ApplicationController
+
+    def index
+        @likes = Like.all
+        render :index
+    end
+
+    def create
+
+        @like = Like.create(like_params)
+
+        if @like.save!
+            render :index
+        end
+            render json: @@like.errors.full_messages, status: 422
+        end
+
+    def userLiked
+        @user = User.find(current_user.id)
+        @likes = @user.likes
+        render :userLiked
+    end
+    
+
+
+    private
+
+    def like_params
+        params.require(:like).permit(:user_id, :chat_id)
+    end
 end
