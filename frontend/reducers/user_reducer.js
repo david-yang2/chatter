d2b91eed91a3c3db29f56741b9d61f7ccc4dfc20
David <david.yang026@gmail.com>
2021-09-22 22:41:56 -0700

    fetching users

---
+++ frontend/reducers/user_reducer.js
@@ -0,0 +1,19 @@
+import {
+    RECEIVE_USER
+} from '../actions/user_actions'
+
+
+const UserReducer = (oldState ={}, action) => {
+    Object.freeze(oldState);
+
+    let nextState = Object.assign({}, oldState);
+
+    switch(action.type){
+        case RECEIVE_USER:
+            return action.user
+        default:
+            return oldState
+    }
+}
+
+export default UserReducer
