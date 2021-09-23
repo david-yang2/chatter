f6d289e09dd649a13836cd0f22a91b0911262075
David <david.yang026@gmail.com>
2021-09-22 21:44:32 -0700

    initial commit

---
+++ frontend/components/profile/profile.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+class Profile extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+
+
+    render(){
+        return(
+            <div>
+                <div>profile</div>
+                <button>Chats</button>
+                <button>LIkes</button>
+            </div>
+        )
+    }
+}
+
+export default Profile
