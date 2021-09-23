6a5fec4fe6038437d734fcdd14138d9a4d58a3ce
David <david.yang026@gmail.com>
2021-09-23 12:37:24 -0700

    getLikes and createLike

---
+++ frontend/actions/like_actions.js
@@ -0,0 +1,24 @@
+import * as LikeApiUtil from "../util/like_api_util"
+
+export const RECEIVE_ALL_LIKES = "RECEIVE_ALL_LIKES"
+
+const receiveLikes = likes => {
+    debugger
+    return {
+        type: RECEIVE_ALL_LIKES,
+        likes
+    }
+}
+
+
+export const getLikes = () => dispatch => {
+    return LikeApiUtil.fetchLikes()
+    .then(likes => dispatch(receiveLikes(likes)))
+}
+
+
+export const createLike = like => dispatch =>{
+    return LikeApiUtil.postLike(like)
+    .then(likes => dispatch(receiveLikes(likes)))
+
+}
