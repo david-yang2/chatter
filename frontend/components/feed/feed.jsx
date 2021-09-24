2a3252455c08ba18d004b8820c152edb986c8105
David <david.yang026@gmail.com>
2021-09-24 13:59:19 -0700

    followPopup closes when clicked outside of button

--- frontend/components/feed/feed.jsx
+++ frontend/components/feed/feed.jsx
@@ -1,19 +1,53 @@
 import React from "react"
 
+import { AiOutlineHeart } from "react-icons/ai";
+import { BsThreeDots } from "react-icons/bs";
 
 class Feed extends React.Component{
     constructor(props){
         super(props)
-    }
+        this.state={
+            toggleFollow: false
+        }
 
+        this.followPopup = this.followPopup.bind(this)
+    }
+    followPopup(bool, e){
+            e.stopPropagation();
+            this.setState({toggleFollow: bool})
+        
+    }
     render(){
         const {chat} = this.props
-
+        let display;
+        if (this.state.toggleFollow === false) {
+            display = (<div></div>)
+        } else {
+            display = (<div className="followContainer">
+                            <button onClick={(e)=> this.followPopup(false,e)}>
+                                follow
+                            </button>
+                        </div>)
+        }
         return(
-            <div className="chat">
+            <div onClick={(e)=>this.followPopup(false,e)} className="feed">
                 {/* avatar, message, likes and replies */}
-                <h5>{chat.username}</h5>
-                <div>{chat.body}</div>
+                <div className="feedHeader">
+                    <div className="userInfo">
+                        <div className="avatar">{/* for avatar */}</div>
+                        <h5>{chat.username}</h5>
+                    </div>
+                    <div>
+                        <button className="followBtn" onClick={(e)=>this.followPopup(true,e)}><BsThreeDots /></button>
+                        {display}
+                    </div> 
+                </div>
+
+                    {/* content */}
+                <div className="feedContent">
+                    <div>{chat.body}</div>
+                    <button onClick={(e)=>this.props.likeChat(chat.id)}><AiOutlineHeart /></button>
+                </div>
             </div>
         )
     }
