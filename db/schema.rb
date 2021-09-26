a6ef52399d8621169b3775816a27f87c8d4e19b7
David <david.yang026@gmail.com>
2021-09-25 23:56:08 -0700

    included topics

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_16_182657) do
+ActiveRecord::Schema.define(version: 2021_09_21_211143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_09_16_182657) do
     t.integer "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "topic"
     t.index ["author_id"], name: "index_chats_on_author_id"
   end
 
