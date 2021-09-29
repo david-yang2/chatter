45736065dc4c9728160a97be3120af1a34c26061
David <david.yang026@gmail.com>
2021-09-29 16:25:50 -0700

    heroku fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_21_211143) do
+ActiveRecord::Schema.define(version: 2021_09_29_171502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 2021_09_21_211143) do
     t.string "email", null: false
     t.string "password_digest"
     t.string "session_token"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["session_token"], name: "index_users_on_session_token"
     t.index ["username"], name: "index_users_on_username", unique: true
   end
