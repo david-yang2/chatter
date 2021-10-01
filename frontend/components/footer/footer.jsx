b16a3a004ab7c84d51738b5ee8ef08b8c4c3c891
David <david.yang026@gmail.com>
2021-09-30 22:36:55 -0700

    included image and content

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -41,13 +41,13 @@ class Footer extends React.Component{
                 <div className="project">
                     <h4>ExplorationAwaits</h4>
                     <p>A full-stack web applicaiton which provides many capable and comfortable overland vehicles that can take you and your family/friends off the beaten path.</p>
-                    <a href="https://explorationawaits.herokuapp.com/" class="image" target="_blank" rel="noopener noreferrer"><img width="100%" src={"/explorationawaits.png"} alt="" /></a>
+                    <a href="https://explorationawaits.herokuapp.com/" target="_blank" rel="noopener noreferrer"><img width="100%" src={"/explorationawaits.png"} alt="" /></a>
                 </div>
 
                 <div className="project">
                     <h4>What's For Dinner?</h4>
                     <p> A full-stack web application which offers a catalog of chefs and items for users to choose from.</p>
-                    <a href="https://explorationawaits.herokuapp.com/" class="image" target="_blank" rel="noopener noreferrer"><img width="100%" src={"/whatsfordinner.png"} alt="" /></a>
+                    <a href="https://explorationawaits.herokuapp.com/" target="_blank" rel="noopener noreferrer"><img width="100%" src={"/whatsfordinner.png"} alt="" /></a>
                     
                 </div>
 
